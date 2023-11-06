f076e817ed7540f115ea717faf55270675b914fd
Boby Kumari <103987147+Boby-1503@users.noreply.github.com>
2023-11-06 12:07:54 +0530

    Add files via upload
    Week 3 Exercise 1

---
+++ src/Ex-1_HelloMaven/src/main/java/org/example/HelloMavenApp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class HelloMavenApp
+{
+    public static void main (String [] args)
+    {
+        System.out.println("Hello, Maven!");
+    }
+
+}
+
+//java -cp target/HelloMaven-1.0-SNAPSHOT.jar org.example.HelloMavenApp-- to run jar file on the terminal
