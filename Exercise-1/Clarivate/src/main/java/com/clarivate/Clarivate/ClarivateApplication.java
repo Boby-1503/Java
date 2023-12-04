bbcd3b52b1a301df9043e46406dcb8c4fe0e8e2e
Boby Kumari <103987147+Boby-1503@users.noreply.github.com>
2023-12-04 14:49:28 +0530

    Week-4,Exercise-1

---
+++ Exercise-1/Clarivate/src/main/java/com/clarivate/Clarivate/ClarivateApplication.java
@@ -0,0 +1,14 @@
+package com.clarivate.Clarivate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarivateApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClarivateApplication.class, args);
+	}
+
+}
